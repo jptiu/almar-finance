643e774409240ec1b98d70709e066891da7bd726
Christian James Simene <christianjamessimeneefiva@gmail.com>
2025-02-09 03:07:24 -0800

    add grace period

--- routes/web.php
+++ routes/web.php
@@ -119,6 +119,7 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::post('loan/import-details', [LoanController::class, 'importCSVDetails'])->name('loan.importcsvdetails');
     Route::post('loan/approve/{id}', [LoanController::class, 'approve'])->name('loan.approve');
     Route::post('loan/decline/{id}', [LoanController::class, 'decline'])->name('loan.decline');
+    Route::post('/update-due-date/{detail}', [LoanController::class, 'updateDueDate'])->name('loan.duedateupdate');
 
     // Collection
     Route::get('collection', [CollectionController::class, 'index'])->name('collection.index');
