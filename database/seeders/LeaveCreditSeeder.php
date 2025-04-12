ca8f768fe0f84a17c63010b581b7628521bf4771
Jhon Patrick Tiu <jhonpatrick.tiu@fligno.com>
2025-04-12 21:40:11 +0800

    fix: Remove role filter from LeaveCreditSeeder
    - Update seeder to create leave credits for all users
    - Remove role-based filtering as it's no longer needed
    - Ensure all users have default leave credits created

--- database/seeders/LeaveCreditSeeder.php
+++ database/seeders/LeaveCreditSeeder.php
@@ -10,7 +10,7 @@ class LeaveCreditSeeder extends Seeder
     public function run()
     {
         // Create default leave credits for all employees
-        $employees = \App\Models\User::where('role', 'employee')->get();
+        $employees = \App\Models\User::get();
         
         foreach ($employees as $employee) {
             LeaveCredit::createDefaultCredits($employee);
