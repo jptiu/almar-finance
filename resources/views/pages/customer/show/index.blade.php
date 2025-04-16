26b7ac8fae4968137e913a18cb6c9be055b1227b
Jhon Patrick Tiu <jhonpatrick.tiu@fligno.com>
2025-04-16 23:22:59 +0800

    fix: Handle potential null value for loan ID in export form action

--- resources/views/pages/customer/show/index.blade.php
+++ resources/views/pages/customer/show/index.blade.php
@@ -97,7 +97,7 @@
                         <div class="flex justify-end">
                             <button type="button" id="close-transaction-btn" class="bg-gray-300 text-gray-700 px-4 py-2 rounded mr-2 hover:bg-gray-400">Cancel</button>
                             <!-- Export Button -->
-                            <form id="export-form" action="{{ route('printcustomerLoan.index', $loan->id) }}" method="GET">
+                            <form id="export-form" action="{{ route('printcustomerLoan.index', $loan->id??'') }}" method="GET">
                                 <input type="hidden" name="transaction_id" id="selected-transaction">
                                 <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
                                     Print
