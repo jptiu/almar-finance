b858f9975e75d6aa773e34a4f0e651ece3e21a69
Jhon Patrick Tiu <jhonpatrick.tiu@fligno.com>
2025-04-16 22:41:22 +0800

    fix: Remove unnecessary navigation links from personal, spousal, company, pensioners, background, and bank account sections

--- resources/views/pages/customer/update/index.blade.php
+++ resources/views/pages/customer/update/index.blade.php
@@ -524,13 +524,6 @@
 
                             <div class="flex justify-between ">
                                 <h3 class="text-2xl font-semibold mb-4">Personal Information</h3>
-                                <a href="step1" class="cursor-pointer">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960"
-                                        width="24px" fill="#434343">
-                                        <path
-                                            d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h357l-80 80H200v560h560v-278l80-80v358q0 33-23.5 56.5T760-120H200Zm280-360ZM360-360v-170l367-367q12-12 27-18t30-6q16 0 30.5 6t26.5 18l56 57q11 12 17 26.5t6 29.5q0 15-5.5 29.5T897-728L530-360H360Zm481-424-56-56 56 56ZM440-440h56l232-232-28-28-29-28-231 231v57Zm260-260-29-28 29 28 28 28-28-28Z" />
-                                    </svg>
-                                </a>
                             </div>
 
                             <div>
@@ -608,13 +601,6 @@
 
                             <div class="flex justify-between ">
                                 <h3 class="text-2xl font-semibold mb-4">Spousal Data</h3>
-                                <a href="step1" class="cursor-pointer">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960"
-                                        width="24px" fill="#434343">
-                                        <path
-                                            d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h357l-80 80H200v560h560v-278l80-80v358q0 33-23.5 56.5T760-120H200Zm280-360ZM360-360v-170l367-367q12-12 27-18t30-6q16 0 30.5 6t26.5 18l56 57q11 12 17 26.5t6 29.5q0 15-5.5 29.5T897-728L530-360H360Zm481-424-56-56 56 56ZM440-440h56l232-232-28-28-29-28-231 231v57Zm260-260-29-28 29 28 28 28-28-28Z" />
-                                    </svg>
-                                </a>
                             </div>
 
                             <div>
@@ -664,13 +650,6 @@
 
                             <div class="flex justify-between ">
                                 <h3 class="text-2xl font-semibold mb-4">Company Information</h3>
-                                <a href="step1" class="cursor-pointer">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960"
-                                        width="24px" fill="#434343">
-                                        <path
-                                            d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h357l-80 80H200v560h560v-278l80-80v358q0 33-23.5 56.5T760-120H200Zm280-360ZM360-360v-170l367-367q12-12 27-18t30-6q16 0 30.5 6t26.5 18l56 57q11 12 17 26.5t6 29.5q0 15-5.5 29.5T897-728L530-360H360Zm481-424-56-56 56 56ZM440-440h56l232-232-28-28-29-28-231 231v57Zm260-260-29-28 29 28 28 28-28-28Z" />
-                                    </svg>
-                                </a>
                             </div>
 
                             <div>
@@ -730,13 +709,6 @@
 
                             <div class="flex justify-between ">
                                 <h3 class="text-2xl font-semibold mb-4">For Pensioners ONLY</h3>
-                                <a href="step1" class="cursor-pointer">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960"
-                                        width="24px" fill="#434343">
-                                        <path
-                                            d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h357l-80 80H200v560h560v-278l80-80v358q0 33-23.5 56.5T760-120H200Zm280-360ZM360-360v-170l367-367q12-12 27-18t30-6q16 0 30.5 6t26.5 18l56 57q11 12 17 26.5t6 29.5q0 15-5.5 29.5T897-728L530-360H360Zm481-424-56-56 56 56ZM440-440h56l232-232-28-28-29-28-231 231v57Zm260-260-29-28 29 28 28 28-28-28Z" />
-                                    </svg>
-                                </a>
                             </div>
 
                             <div>
@@ -767,13 +739,6 @@
 
                             <div class="flex justify-between ">
                                 <h3 class="text-2xl font-semibold mb-4">Background Data</h3>
-                                <a href="step1" class="cursor-pointer">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960"
-                                        width="24px" fill="#434343">
-                                        <path
-                                            d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h357l-80 80H200v560h560v-278l80-80v358q0 33-23.5 56.5T760-120H200Zm280-360ZM360-360v-170l367-367q12-12 27-18t30-6q16 0 30.5 6t26.5 18l56 57q11 12 17 26.5t6 29.5q0 15-5.5 29.5T897-728L530-360H360Zm481-424-56-56 56 56ZM440-440h56l232-232-28-28-29-28-231 231v57Zm260-260-29-28 29 28 28 28-28-28Z" />
-                                    </svg>
-                                </a>
                             </div>
 
                             <div>
@@ -808,13 +773,6 @@
 
                             <div class="flex justify-between ">
                                 <h3 class="text-2xl font-semibold mb-4">Bank Account Informationsn</h3>
-                                <a href="step1" class="cursor-pointer">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960"
-                                        width="24px" fill="#434343">
-                                        <path
-                                            d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h357l-80 80H200v560h560v-278l80-80v358q0 33-23.5 56.5T760-120H200Zm280-360ZM360-360v-170l367-367q12-12 27-18t30-6q16 0 30.5 6t26.5 18l56 57q11 12 17 26.5t6 29.5q0 15-5.5 29.5T897-728L530-360H360Zm481-424-56-56 56 56ZM440-440h56l232-232-28-28-29-28-231 231v57Zm260-260-29-28 29 28 28 28-28-28Z" />
-                                    </svg>
-                                </a>
                             </div>
 
                             <div>
